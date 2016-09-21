ad099e1becca4f7c7373d5e301adc607b4488497
asher <asherbare@gmail.com>
2016-09-21 22:06:33 +0300

    Added setup.py script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyzipit',
+    version='0.01',
+    packages=['zip_by_address', 'zip_by_address.zip_by_address_localizations'],
+    url='https://github.com/asherbar/pyzipit',
+    license='MIT',
+    author='asherbar',
+    author_email='asherbare@gmail.com',
+    description='A Python library that provides tools for ZIP (as in "Zone Improvement Plan") related subjects'
+)
