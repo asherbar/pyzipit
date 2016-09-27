e657a5b6efb2b2e4804d41d7143c5beff88e2b04
asher <asherbare@gmail.com>
2016-09-27 22:31:36 +0300

    Finalized implementation of address_by_coordinates_resolver.py

--- zip_by_address/address_by_coordinates/address_by_coordinates_resolver.py
+++ zip_by_address/address_by_coordinates/address_by_coordinates_resolver.py
@@ -1,12 +1,41 @@
-import requests
+import io
+import json
+import os
+import urllib.request
+
+from zip_by_address.address import Address
 
 
 class AddressByCoordinatesResolver:
-    def __init__(self):
-        self.latitude = -1
-        self.longitude = -1
+    _GM__REVERSE_JSON_ADDRESS = 'https://maps.googleapis.com/maps/api/geocode/json?&latlng'
+
+    def __init__(self, language='en', latitude=-1, longitude=-1):
+        self.language = language
+        self.latitude = latitude
+        self.longitude = longitude
 
     def resolve_address(self):
-        _GM__REVERSE_JSON_ADDRESS = 'https://maps.googleapis.com/maps/api/geocode/json?latlng'
-        requests.get(
-            '%s=32.1091841,34.8354139&key=AIzaSyCds4p3MBAOso2f1cJ-yRo3t-S3nUQNRIM&result_type=street_address&language=he' % _GM__REVERSE_JSON_ADDRESS)
+        request_url = '{}={},{}&key={}&language={}'.format(
+            self._GM__REVERSE_JSON_ADDRESS, self.latitude, self.longitude, os.environ['GOOGLE_MAPS_API_KEY'],
+            self.language)
+        with urllib.request.urlopen(request_url) as response, \
+                io.TextIOWrapper(response, encoding=response.headers.get_content_charset('utf-8')) as f:
+            result = json.load(f)
+        if result['status'] != 'OK':
+            raise RuntimeError('Unable to get address from coordinates ({}, {}). Request ended with status {}'.format(
+                self.latitude, self.longitude, result['status'])
+            )
+        address_components = result['results'][0]['address_components']
+        country, state, city, street, street_number = [''] * 5
+        for component in address_components:
+            if 'street_number' in component['types']:
+                street_number = int(component['long_name'].split('-')[0])
+            elif 'route' in component['types']:
+                street = component['long_name']
+            elif 'locality' in component['types']:
+                city = component['long_name']
+            elif 'administrative_area_level_1' in component['types']:
+                state = component['long_name']
+            elif 'country' in component['types']:
+                country = component['long_name']
+        return Address(country, state, city, street, street_number)
