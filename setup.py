ab9f74e17aeae5cc683f3a3c0ea2be28c0d3edc4
asher.bar.eitan <asher@redislabs.com>
2016-09-27 13:00:31 +0300

    Fixed setup.py: version and url entries were incorrect

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup
 
 setup(
     name='pyzipit',
-    version='0.02',
+    version='0.0.2',
     packages=['zip_by_address', 'zip_by_address.zip_by_address_localizations'],
-    url='https://github.com/asherbar/pyzipit',
+    url='https://github.com/asherbar/pyzipit/tarball/0.0.2',
     license='MIT',
     author='asherbar',
     author_email='asherbare@gmail.com',
