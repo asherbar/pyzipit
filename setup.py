a412358443e0611ca580b9628345430fbf0cfbf4
asher <asherbare@gmail.com>
2016-09-30 20:47:23 +0300

    Updated version: 0.0.4 --> 0.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pyzipit',
-    version='0.0.4',
+    version='0.0.5',
     packages=[
         'zip_by_address',
         'zip_by_address.zip_by_address_localizations',
@@ -12,7 +12,7 @@ setup(
         'zip_by_address.address_by_coordinates.address_by_coordinates_localizations.israel',
         'zip_by_address.coordinates_by_address',
     ],
-    url='https://github.com/asherbar/pyzipit/tarball/0.0.4',
+    url='https://github.com/asherbar/pyzipit/tarball/0.0.5',
     license='MIT',
     author='asherbar',
     author_email='asherbare@gmail.com',
